2c2c8c72adf2c067251e68b5edab541ca78e0b88
Maks <3abecc@gmail.com>
2022-09-24 20:03:45 +0300

    2chenged

--- pr2/main.py
+++ pr2/main.py
@@ -1,6 +1,6 @@
 import math
-x = float(input("Введите значение"))
-y = float(input("Введите значение"))
-z = float(input("Введите значение"))
-s = 2 ** -x * sqrt(x + (abs(y)**0.25))
-print("s = {0:.2f}".format(s))
+x = float(input("x"))
+y = float(input("y"))
+z = float(input("z"))
+s = (math.e **(math.fabs(x - y))) * ((math.fabs (x - y)) **(x + y)) / (math.atan(x) + math.atan(z)) + (x**6 + math.log(y)**2)**(1/3)
+print("s= {0:.4f}".format (s))
