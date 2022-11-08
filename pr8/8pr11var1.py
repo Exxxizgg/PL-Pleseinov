48eccd3950abc3d62bfae23b8960158fd26a40ce
Max <3abecc@gmail.com>
2022-11-08 14:35:45 +0300

    8prvar11Num1

--- pr8/8pr11var1.py
+++ pr8/8pr11var1.py
@@ -1,19 +1,23 @@
-P=int(input('Введите порядок матрицы: '))
-A=[[int(input()) for j in range(P)] for i in range(P)]
-print('Итоговая матрица: ')
-for i in range(P):
-    for j in range(P):
-        print(A[i][j],end=" ")
-print()
-m=10**10
+from random import randint
+n=int(input())
+a=[[0 for i in range(n)]for j in range(n)]
+for i in range(n):
+    for j in range(n):
+        a[i][j]=randint(-10,10)
+    print()
+for i in range(n):
+    for j in range(n):
+        print(a[i][j],end=" ")
+    print()
+m=100
 k=0
-for i in range(P):
-    for j in range(P):
-        if A[i][j]<m:
-            m=A[i][j]
+for i in range(n):
+    for j in range(n):
+        if a[i][j]<m:
+            m=a[i][j]
             k=i
             c=0
-print('Ответ: ')
-for j in range(P):
-    c+=A[k][j]
+print('Cумма элементов строки,имеющей наименьший элемент : ')
+for j in range(n):
+    c+=a[k][j]
 print(c)
