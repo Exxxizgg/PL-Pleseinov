d1af5c0aa15c98bd013f71f5c3702d95c2c76cc7
Max <3abecc@gmail.com>
2022-11-08 19:11:51 +0300

    9prVar1addNum2

--- pr9/Var11/2/11varNum2.py
+++ pr9/Var11/2/11varNum2.py
@@ -1,50 +1,19 @@
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
 from random import randint
 from operator import mul
 from functools import reduce
-
+matrix = []
+i = 0
+n = 3
+f = open("11varNum2.txt","r")
+for line in f:
+    matrix.append([int(x) for x in line.split()])
+print(matrix)
 
 def mprint(matrix):
     for row in matrix:
         for item in row:
             print(f'{item:>3}', end=' ')
         print()
-
-
-n = int(input('Size: '))
-
-matrix = [[randint(-10, 10) for _ in range(n)] for _ in range(n)]
-mprint(matrix)
-print()
-
 tmatrix = list(zip(*matrix))
 ps = [reduce(mul, row) for row in tmatrix]
 min_p = min(ps)
@@ -56,4 +25,5 @@ else:
     tmatrix[idx], tmatrix[idx - 1] = tmatrix[idx - 1], tmatrix[idx]
 
 matrix = list(zip(*tmatrix))
-mprint(matrix)
+f = open("ЗаписьNum2.txt", "a")
+f.write(str(matrix))
