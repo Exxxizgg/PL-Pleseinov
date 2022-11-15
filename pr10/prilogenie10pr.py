60a809b63f2b0ee8d4c23cedf6bf4aaa11c991d8
Max <3abecc@gmail.com>
2022-11-15 09:59:26 +0300

    10pr

--- pr10/prilogenie10pr.py
+++ pr10/prilogenie10pr.py
@@ -9,7 +9,7 @@ BUTTON_FONT = ("Arial", 25)
 
 root=Tk()
 root.title("Плесеинов Максим Михайлович")
-root.geometry("400x430")
+root.geometry("600x430")
 root.resizable(False,False)
 
 tab_control = ttk.Notebook(root)
@@ -40,39 +40,39 @@ def calculate():
 
 # дисплей
 input_text = tk.StringVar()
-display = tk.Entry(tab1, font=DISPLAY_FONT, textvariable=input_text, bg="azure", fg="black", width=12)
+display = tk.Entry(tab1, font=DISPLAY_FONT, textvariable=input_text, bg="azure", fg="black", width=28)
 display.grid(row=0, columnspan=4, ipady=10)
 
 # Кнопки
 # кнопки1
-tk.Button(tab1, text="(", font=BUTTON_FONT, fg="black", bg="medium aquamarine", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("("))).grid(row=1, column=0)
-tk.Button(tab1, text=")", font=BUTTON_FONT, fg="black", bg="medium aquamarine", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(")"))).grid(row=1, column=1)
-tk.Button(tab1, text="С", font=BUTTON_FONT, fg="black", bg="medium aquamarine", width=3, height=1, command=lambda: display.delete(len(display.get())-1)).grid(row=1, column=2)
-tk.Button(tab1, text="=", font=BUTTON_FONT, fg="black", bg="light blue", width=3, height=1, command=calculate).grid(row=1, column=3)
+tk.Button(tab1, text="(", font=BUTTON_FONT, fg="black", bg="medium aquamarine", width=5, height=1, command=lambda: display.insert(tk.INSERT, str("("))).grid(row=1, column=0)
+tk.Button(tab1, text=")", font=BUTTON_FONT, fg="black", bg="medium aquamarine", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(")"))).grid(row=1, column=1)
+tk.Button(tab1, text="С", font=BUTTON_FONT, fg="black", bg="medium aquamarine", width=5, height=1, command=lambda: display.delete(len(display.get())-1)).grid(row=1, column=2)
+tk.Button(tab1, text="=", font=BUTTON_FONT, fg="black", bg="light blue", width=5, height=1, command=calculate).grid(row=1, column=3)
 
 # кнопки2
-tk.Button(tab1, text="1", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(1))).grid(row=2, column=0)
-tk.Button(tab1, text="2", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(2))).grid(row=2, column=1)
-tk.Button(tab1, text="3", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(3))).grid(row=2, column=2)
-tk.Button(tab1, text="+", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("+"))).grid(row=2, column=3)
+tk.Button(tab1, text="1", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(1))).grid(row=2, column=0)
+tk.Button(tab1, text="2", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(2))).grid(row=2, column=1)
+tk.Button(tab1, text="3", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(3))).grid(row=2, column=2)
+tk.Button(tab1, text="+", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str("+"))).grid(row=2, column=3)
 
 # кнопки3
-tk.Button(tab1, text="4", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(4))).grid(row=3, column=0)
-tk.Button(tab1, text="5", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(5))).grid(row=3, column=1)
-tk.Button(tab1, text="6", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(6))).grid(row=3, column=2)
-tk.Button(tab1, text="-", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("-"))).grid(row=3, column=3)
+tk.Button(tab1, text="4", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(4))).grid(row=3, column=0)
+tk.Button(tab1, text="5", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(5))).grid(row=3, column=1)
+tk.Button(tab1, text="6", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(6))).grid(row=3, column=2)
+tk.Button(tab1, text="-", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str("-"))).grid(row=3, column=3)
 
 # кнопки4
-tk.Button(tab1, text="7", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(7))).grid(row=4, column=0)
-tk.Button(tab1, text="8", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(8))).grid(row=4, column=1)
-tk.Button(tab1, text="9", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(9))).grid(row=4, column=2)
-tk.Button(tab1, text="x", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("*"))).grid(row=4, column=3)
+tk.Button(tab1, text="7", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(7))).grid(row=4, column=0)
+tk.Button(tab1, text="8", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(8))).grid(row=4, column=1)
+tk.Button(tab1, text="9", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(9))).grid(row=4, column=2)
+tk.Button(tab1, text="x", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str("*"))).grid(row=4, column=3)
 
 # кнопки5
-tk.Button(tab1, text="C", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: input_text.set("")).grid(row=5, column=0)
-tk.Button(tab1, text="0", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str(0))).grid(row=5, column=1)
-tk.Button(tab1, text=".", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("."))).grid(row=5, column=2)
-tk.Button(tab1, text="/", font=BUTTON_FONT, fg="black", bg="plum1", width=3, height=1, command=lambda: display.insert(tk.INSERT, str("/"))).grid(row=5, column=3)
+tk.Button(tab1, text="C", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: input_text.set("")).grid(row=5, column=0)
+tk.Button(tab1, text="0", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str(0))).grid(row=5, column=1)
+tk.Button(tab1, text=".", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str("."))).grid(row=5, column=2)
+tk.Button(tab1, text="/", font=BUTTON_FONT, fg="black", bg="plum1", width=5, height=1, command=lambda: display.insert(tk.INSERT, str("/"))).grid(row=5, column=3)
 
 services = []
 
